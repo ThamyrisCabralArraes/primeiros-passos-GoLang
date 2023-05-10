9a09f9c7da6268a8a0ba6eb814009664a4caffc5
thamyris <thamyriscabral@hotmail.com>
2023-05-10 17:57:35 -0300

    feat: variaveis

---
+++ variaveis.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func soma(x, y int) int {
+	return x + y
+}
+
+func calcular(a int) (int, int) {
+	var quadrado = a * a
+	var cubo = a * a * a
+
+	return quadrado, cubo
+}
+//pode ser das duas maneiras, só em funcoes curtas
+func calcular2(a int) (quadrado int, cubo int) {
+	quadrado = a * a
+	cubo = a * a * a
+
+	return quadrado, cubo
+}
+
+func main() {
+	const (
+		nome string = "Thamyris"
+		idade int = 34
+		aberto = true
+	)
+
+var Numero = math.Max(3, 5) // nao pega com const
+	
+fmt.Println(calcular(2))
+	fmt.Println("maior numero", Numero)
+	fmt.Println("meu nome é", nome, "tenho", idade, "anos")
+	fmt.Println("valor da soma é", soma(2, 3))
+
+	fmt.Printf("tipo: %T Valor: %v", aberto, nome)
+
+}
